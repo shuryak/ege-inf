b07f4323937aa2cee48e05327d162fae95a63458
Alexander Konovalov <shuryak@protonmail.com>
2021-07-02 12:50:56 +0300

    fix: kege 25

--- kege/25.py
+++ kege/25.py
@@ -1,19 +1,30 @@
 # https://inf-ege.sdamgia.ru/problem?id=35483
 
-for n in range(35000000, 40000000+1): 
-    if n % 2 != 0: # Если число не делится на 2
-        a = [1,n] # Значит 2 делителя: 1 и число n
-    else: a = [1] # 1 делитель: число 1
+def find(number):
+    if number % 2 != 0: # Если число number не делится на 2
+        a = [1, number] # Значит 2 нечётных делителя: 1 и само число number
+    else: a = [1] # Иначе 1 нечётный делитель: число 1
 
+    d = 2
+    while d * d <= number:
+        if number % d == 0: # Проверяем делимость
+            if d % 2 != 0: # Нечётный делитель
+                a.append(d) # Добавляем нечётный делитель
+            if number // d != d and (number // d) % 2 != 0: # Исключаем квадрат и проверяем на нечётность парный делитель
+                a.append(number // d) # Добавляем нечетный парный делитель
+        d += 1
+    
+    return a
+
+result = []
+
+for n in range(35000000, 2 * 40000000 + 1): 
     if int(n ** 0.5)**2 == n: # Действия ниже выполянем только для квадратов
-        d = 2
-        while d * d <= n:
-            if n % d == 0: # Проверяем делимость
-                if d % 2 != 0: # Нечётный делитель
-                    a.append(d)# Добавляем нечётный делитель
-                if n // d != d and (n // d) % 2 != 0: # Исключаем квадрат
-                    a.append(n // d)# Добавляем нечетный парный делитель
-            if len(a)>5: break
-            d += 1
-        if len(a) == 5: 
-            print(n) 
+        if len(find(n)) > 5:
+            continue
+        if n < 40000000 and len(find(n)) == 5:
+            result.append(n)
+        if n // 2 > 35000000 and n // 2 < 40000000 and len(find(n // 2)) == 5:
+            result.append(n // 2)
+
+print(sorted(result))
